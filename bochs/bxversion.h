4b1f0d0b88d466b45d0c40fc37d9bb4bdf5aedc1
bstellaceleste <bstellaceleste@yahoo.fr>
2022-05-24 13:16:42 +0200

    bochs

---
+++ bochs/bxversion.h
@@ -0,0 +1,9 @@
+/////////////////////////////////////////////////////////////////////////
+// This file is checked in as bxversion.h.in.  The configure script
+// substitutes variables and creates bxversion.h.
+/////////////////////////////////////////////////////////////////////////
+
+#define VERSION       "2.6.11.svn"
+#define VER_SVNFLAG   1
+#define REL_STRING    "Built from SVN snapshot after release 2.6.11"
+#define REL_TIMESTAMP "Sun Jan  5 08:36:00 CET 2020"
---
+++ bochs/myconfigure.sh
@@ -0,0 +1,6 @@
+./configure --enable-x86-64 --enable-vmx=2 --enable-cpu-level=6 --enable-es1370\
+--with-all-libs --enable-e1000 --enable-ltdl-install --enable-plugins --enable-ne2000\
+--with-term\
+--enable-smp\
+--enable-all-optimizations\
+--enable-avx --enable-evex --enable-long-phy-address
